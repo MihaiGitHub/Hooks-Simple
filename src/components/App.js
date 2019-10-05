7157d497d1823974a56708eaab01eeca7fc41072
Mihai Smarandache <mihai.sanfran@gmail.com>
2019-10-04 17:08:55 -0700

    Add useState to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-        return (
+    // array destructure; names can be any name you want to call it
+    // resource - contains the present value of this piece of state
+    // setResource - same as calling setState; component will rerender
+    // useState('posts') - set up state with initial value of posts
+    const [resource, setResource] = useState('posts');
+
+    return (
+        <div>
             <div>
-                <div>
-                    <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
-                    <button onClick={() => this.setState({resource: 'todos'})}>Todos</button>
-                </div>
-                {this.state.resource}
+                <button onClick={() => setResource('posts')}>Posts</button>
+                <button onClick={() => setResource('todos')}>Todos</button>
             </div>
-        );
+            {resource}
+        </div>
+    );
 }
 
 export default App;
